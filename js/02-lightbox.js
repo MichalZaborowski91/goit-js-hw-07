7e00aa015317a9c9a3566ae345aa07308fa8e7c2
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-22 11:36:15 +0100

    02 lightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGallery(items) {
+  return items
+    .map(
+      (item) => `<li><a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a></li>`
+    )
+    .join("");
+}
+gallery.innerHTML = createGallery(galleryItems);
+
+//Vanilla JavaScript Plugin
+const lightbox = new SimpleLightbox(".gallery a", {
+  //Options
+  captionsData: "alt",
+});
