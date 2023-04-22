72f726742965bfa6c52c855d0dca80c48a60646e
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-22 08:48:01 +0100

    01 gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const divGallery = document.querySelector(".gallery");
+
+function createGallery(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+divGallery.innerHTML = createGallery(galleryItems);
+divGallery.addEventListener("click", imageClick);
+
+function imageClick(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+  divGallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
